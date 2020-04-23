b6f0fe20aa34f2bb9911b3f9713d1f86fd8f8520
umeshb <umesh.bhatt25@gmail.com>
2020-04-23 16:22:13 +0530

    Added calculator challenge

---
+++ calculator/src/main/java/com/learning/sample/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.learning.sample.calculator;
+
+public class Calculator {
+	
+	public static int add(int x, int y) {
+		//please implement add logic here
+		return x+y;
+	}
+	
+	public static int subtract(int x, int y) {
+		//plese implement subtract logic here
+		return x-y;
+	}
+}
---
+++ calculator/src/test/java/com/learning/sample/calculator/AppTest.java
@@ -0,0 +1,50 @@
+package com.learning.sample.calculator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp1()
+    {
+    	int z = Calculator.add(2, 3);
+    	assertEquals(5, z);
+    }
+    
+    public void testApp2()
+    {
+    	int z = Calculator.add(7, 3);
+    	assertEquals(10, z);
+    }
+    
+    public void testApp3()
+    {
+    	int z = Calculator.subtract(5, 3);
+    	assertEquals(2, z);
+    }
+}
