831c0a4c4b9103c33c47f873562b2c0b82643313
umeshbhatt25 <umesh.bhatt25@gmail.com>
2020-06-21 22:18:22 +0530

    Add files via upload

---
+++ calculator/src/main/java/com/learning/sample/calculator/JsonParser.java
@@ -0,0 +1,22 @@
+package com.learning.sample.calculator;
+
+//import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonParser {
+	
+	public static String toString(Object obj, boolean pascalCase) {
+		//
+		// Start implementing logic here
+		//
+		/*try {
+			 ObjectMapper mapper = new ObjectMapper(); 
+			 String json = mapper.writeValueAsString(obj);
+			 return json;
+		} catch (Exception ex) {
+			return "";
+		}*/
+		
+		return "";
+	}
+}
